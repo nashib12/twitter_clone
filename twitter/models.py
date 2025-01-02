5ac8092c60ae60d37ed85da22870ba9a59ffb80c
Nashib Gurung <nashibgurung46@gmail.com>
2025-01-02 14:46:41 +0545

    first commit

---
+++ twitter/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class StudentForm(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.CharField(max_length=200)
+    phonemumber = models.IntegerField()
+    message = models.TextField()
+    
